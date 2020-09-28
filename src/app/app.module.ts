05fd4726a6bb027a2376c1dff6f0489f34715590
kainthsourav <souravkainth@outlook.com>
2020-09-28 10:34:57 +0530

    Added Routing .. ng g m routing --routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { AppHomeComponent } from './app-home/app-home.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    AppHomeComponent,
   ],
   imports: [
     BrowserModule
