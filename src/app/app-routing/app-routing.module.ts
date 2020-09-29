397cf7a920dce70fae2a150f61d822711fafbc70
kainthsourav <souravkainth@outlook.com>
2020-09-29 18:46:35 +0530

    Recipe Project --Componets added --Header added

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import{Routes,RouterModule} from '@angular/router';
 import { AppHomeComponent } from '../app-home/app-home.component';
+import { AppComponent } from '../app.component';
 
 
 const route:Routes=[
-  {path:'',component:AppHomeComponent}
+  {path:'',component:AppComponent}
 ]
 @NgModule({
   declarations: [],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,30 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { AppHomeComponent } from './app-home/app-home.component';
 import{AppRoutingModule} from './app-routing/app-routing.module';
 import{FormsModule,ReactiveFormsModule} from  '@angular/forms';
+import { HeaderComponent } from './header/header.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { RecipeBookComponent } from './recipe-book/recipe-book.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipesListComponent } from './recipes/recipes-list/recipes-list.component';
+import { RecipesDetailsComponent } from './recipes/recipes-details/recipes-details.component';
+import { RecipeItemComponent } from './recipes/recipes-list/recipe-item/recipe-item.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
-    AppHomeComponent
+    AppHomeComponent,
+    HeaderComponent,
+    ShoppingListComponent,
+    RecipeBookComponent,
+    RecipesComponent,
+    RecipesListComponent,
+    RecipesDetailsComponent,
+    RecipeItemComponent,
+    ShoppingEditComponent
   ],
   imports: [
     BrowserModule,
