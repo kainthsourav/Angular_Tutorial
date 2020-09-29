09c1c036b21c3dbfc789853eabc48acbb91c6cc2
kainthsourav <souravkainth@outlook.com>
2020-09-29 19:33:41 +0530

    Show Recipe

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipes-list',
@@ -7,8 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipesListComponent implements OnInit {
 
-  recipes:[];
-  
+  recipes:Recipe[]=[
+    new Recipe('This is a Test Recipe','test desc','https://cdn.loveandlemons.com/wp-content/uploads/2020/03/pantry-recipes-2.jpg')
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
